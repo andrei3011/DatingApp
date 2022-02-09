fc06290ec9d6533c7469922c21769c73ecd634fc
andrei3011 <p.andreidaniel@yahoo.com>
2022-02-09 11:58:48 +0200

    End of section 12

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,6 +5,9 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule, BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { defineLocale, roLocale } from 'ngx-bootstrap/chronos';
+defineLocale('ro', roLocale);
 
 @NgModule({
   declarations: [],
@@ -17,6 +20,7 @@ import { FileUploadModule } from 'ng2-file-upload';
     TabsModule.forRoot(),
     NgxGalleryModule,
     FileUploadModule,
+    BsDatepickerModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -24,6 +28,11 @@ import { FileUploadModule } from 'ng2-file-upload';
     TabsModule,
     NgxGalleryModule,
     FileUploadModule,
+    BsDatepickerModule,
   ],
 })
-export class SharedModule {}
+export class SharedModule {
+  constructor(private bsLocaleService: BsLocaleService) {
+    this.bsLocaleService.use('ro'); //romanian datepicker
+  }
+}
